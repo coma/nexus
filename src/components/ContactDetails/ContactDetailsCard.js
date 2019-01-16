9b1766a39a0beee075f74a25f41c080cd0a63190
coma <eduardo@comakai.com>
2019-01-16 01:52:09 +0100

    a fancy card showing contact details (I'm falling asleep by now)

--- src/components/ContactDetails/ContactDetailsCard.js
+++ src/components/ContactDetails/ContactDetailsCard.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ContactDetailsCard(props) {
-  return <h2>{props.contact.name.first}</h2>;
+import {
+  Wrapper,
+  Picture,
+  Name,
+  Details,
+  Email,
+  Phone,
+} from './ContactDetailsCard.style';
+
+function ContactDetailsCard({ contact }) {
+  return (
+    <Wrapper>
+      <Picture sizes={contact.picture} />
+      <Name>
+        {contact.name.title} 
+        {' '}
+        {contact.name.first} 
+        {' '}
+        {contact.name.last}
+      </Name>
+      <Details>
+        <Email href={`mailto:${contact.email}`}>{contact.email}</Email>
+        <Phone href={`tel:${contact.phone}`}>{contact.phone}</Phone>
+      </Details>
+    </Wrapper>
+  );
 }
 
 ContactDetailsCard.propTypes = {
   contact: PropTypes.shape({
     name: PropTypes.shape({
-      first: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      first: PropTypes.string,
+      last: PropTypes.string,
     }).isRequired,
+    picture: PropTypes.shape({
+      large: PropTypes.string,
+      medium: PropTypes.string,
+      thumbnail: PropTypes.string,
+    }).isRequired,
+    email: PropTypes.string,
+    phone: PropTypes.string,
   }).isRequired,
 };
 
---
+++ src/components/ContactDetails/ContactDetailsCard.style.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { rgba } from 'polished';
+
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  width: 600px;
+  max-width: 100vw;
+  padding: 24px;
+  border: 1px solid ${props => rgba(props.theme['--color-dark'], 0.1)};
+  border-radius: 0.4rem;
+  background-color: ${props => props.theme['--color-light']};
+`;
+
+export const Info = styled.div`
+  padding: 16px;
+`;
+
+export const Name = styled.div`
+  ${props => props.theme['--font-extra-large']};
+  margin: 1em 0;
+  padding-bottom: 0.5em;
+  text-align: center;
+  margin-bottom: 1m;
+  border-bottom: 1px solid ${props => rgba(props.theme['--color-dark'], 0.1)};
+  text-transform: capitalize;
+`;
+
+export const Details = styled.div`
+  & > * + * {
+    margin-top: 1rem;
+  }
+`;
+
+export const Email = styled.a`
+  display: block;
+  ${props => props.theme['--font-large']};
+
+  &::before {
+    content: '✉️ ';
+  }
+`;
+
+export const Phone = styled.a`
+  display: block;
+  ${props => props.theme['--font-large']};
+
+  &::before {
+    content: '☎️ ';
+  }
+`;
+
+export const Picture = styled.div`
+  font-size: 48px;
+  width: 1em;
+  height: 1em;
+  margin: 0 auto;
+  border-right: 1px solid ${props => rgba(props.theme['--color-dark'], 0.1)};
+  border-radius: 50%;
+  background-color: red;
+  background-image: url(${props => props.sizes.medium});
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: cover;
+
+  @media (${props => props.theme['--screen-small']}) {
+    font-size: 128px;
+    background-image: url(${props => props.sizes.large});
+  }
+`;
+
+Picture.propTypes = {
+  sizes: PropTypes.shape({
+    large: PropTypes.string,
+    medium: PropTypes.string,
+  }).isRequired,
+};
